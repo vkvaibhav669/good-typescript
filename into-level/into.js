9082651e67782523dc71cdf70ffb7f376eb2cb6b
vkvaibhavtb <vaibhavkumar_rajkumar@srmuniv.edu.in>
2023-07-10 21:24:59 +0530

    YT stuff

---
+++ into-level/into.js
@@ -0,0 +1,6 @@
+var con = 12;
+var concat = "Hello String";
+console.log("Javascript");
+var user = { name: "Vaibhav", age: 25 };
+console.log(user);
+console.log(user.name);
---
+++ into-level/into.ts
@@ -0,0 +1,12 @@
+let con : number = 12;
+
+let concat : string = "Hello String";
+
+console.log("Javascript");
+
+
+let user = {name :"Vaibhav", age : 25};
+
+console.log(user);
+
+console.log("User name",user.name);
