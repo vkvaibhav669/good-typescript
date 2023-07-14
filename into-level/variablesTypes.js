b0bf372a5efc90e6d656bd641e63ea52a729d180
vkvaibhavtb <vaibhavkumar_rajkumar@srmuniv.edu.in>
2023-07-14 11:25:52 +0530

    updated learning typo again

---
+++ into-level/variablesTypes.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greeeting = "hello vaibhav ";
+console.log(greeeting);
---
+++ into-level/variablesTypes.ts
@@ -0,0 +1,8 @@
+let greeeting : string = "hello vaibhav ";
+
+console.log(greeeting);
+greeeting = "Hello world";
+console.log(greeeting);
+greeeting.toLowerCase()
+console.log(greeeting);
+export {}
